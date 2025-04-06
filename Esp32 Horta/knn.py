c5e4370f784f6fb4dac95a69706b2589a21d939f
Akumas <65172725+Samuks666@users.noreply.github.com>
2025-04-06 11:32:55 -0300

    Update knn.py
    Treinamento base da IA com o algoritmo de KNN atualizada

--- Esp32 Horta/knn.py
+++ Esp32 Horta/knn.py
@@ -6,21 +6,22 @@ from sklearn.neighbors import KNeighborsClassifier
 import pickle
 
 # Carregar o dataset
-url = "TARP.csv"  # Substitua pelo caminho correto do seu arquivo CSV
+url = "TARP.csv"
 df = pd.read_csv(url)
 
-# Converter o status 'on' e 'off' para 1 e 0
+# Converter o status 'ON' e 'OFF' para 1 e 0 (dados númericos)
 df['Status'] = df['Status'].apply(lambda x: 1 if x == 'ON' else 0)
 
 # Selecionar as colunas de características e o rótulo
-X = df[['Soil Moisture', 'Temperature', 'Soil Humidity Time',
-        'Air temperature (C)', 'Air humidity (%)', 'Pressure (KPa)', 'rainfall']].values
+X = df[['Soil Moisture', 'Temperature', 
+        'Soil Humidity', 'Air temperature (C)', 
+        'Air humidity (%)', 'Pressure (KPa)', 'rainfall']].values
 y = df['Status'].values  # Rótulo (1 = Regar, 0 = Não regar)
 
-# Dividir os dados em conjunto de treino e teste
+# Dividir os dados em conjunto de treino e teste (20% de teste)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Padronizar os dados
+# Padronizar os dados (Normalização)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -34,6 +35,7 @@ accuracy = knn.score(X_test, y_test)
 print(f'Acurácia do modelo: {accuracy * 100:.2f}%')
 
 # Salvar o modelo e os dados de treino em arquivos binários
+# Para a leitura no esp32 (em teste)
 with open('knn_model.pkl', 'wb') as model_file:
     pickle.dump(knn, model_file)
 
@@ -44,6 +46,6 @@ with open('scaler.pkl', 'wb') as scaler_file:
 np.save('X_train.npy', X_train)
 np.save('y_train.npy', y_train)
 
-# Se necessário, você também pode salvar as previsões
+# Salva as previsões (Talvez use não sei ainda)
 predictions = knn.predict(X_test)
 np.save('predictions.npy', predictions)
