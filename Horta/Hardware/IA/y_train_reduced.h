e1753fdd25d8d907c1074a3071419bdbd89e85fd
Akumas <65172725+Samuks666@users.noreply.github.com>
2025-05-06 20:46:42 -0300

    Delete Horta/Hardware/IA/y_train_reduced.h

--- Horta/Hardware/IA/y_train_reduced.h
+++
@@ -1,8 +0,0 @@
-#ifndef Y_TRAIN_REDUCED_H
-#define Y_TRAIN_REDUCED_H
-
-static const int y_train_reduced[] = {
-0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0
-};
-
-#endif // Y_TRAIN_REDUCED_H
